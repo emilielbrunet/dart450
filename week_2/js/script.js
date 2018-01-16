44a9e1dd6a6ceb94857c73964bd4caf4a0783a1e
emiliebrunet <emilie.brunet.97@gmail.com>
2018-01-16 14:17:59 -0500

    Changing up the js

---
+++ week_2/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+$('#exampleDiv').text('Disaster!');
+$('.exampleClass').text('Disaster!');
+
+$('.exampleClass').css({
+  color: 'red',
+  fontSize: '10em',
+  backgroundColor: 'yellow'
+});
+
+
+
+
+});
