d7571ed79fa24b319255495a90d1257f4eadf41d
emilielbrunet <emilie.brunet.97@gmail.com>
2018-01-20 10:53:31 -0500

    Trying to fix js
    Trying to make butterfly divs toggle at interval rates

--- Exercises/exercise02/js/script.js
+++ Exercises/exercise02/js/script.js
@@ -8,18 +8,20 @@ Description of what the script does...
 
 **********************************************/
 
-$(document).ready() {
-
-  $(function () {
-      $('#inputDatabaseName').keypress(function () {
-          var $this = $(this),
-              $div = $(this).parent();
-          if ($this.val().length > 0) {
-              $div.addClass("hasContent");
-          } else {
-              $div.removeClass("hasContent");
-          }
-      });
-  });
-
+	$(document).ready(function () {
+var myVar = setInterval(function(){ setOpacity() }, 100);
+ 
+function setOpacity() {
+  var x = document.getElementById("#bf3");
+  x.style.opacity = x.style.opacity ===
+"0" ? "1" : "0";
 }
+				 
+function stopOpacity() {
+  clearInterval(myVar);
+}
+ 
+//		
+//		         function reduceOpacity() {
+//            $('test').setOpacity( 0.5 );
+});
