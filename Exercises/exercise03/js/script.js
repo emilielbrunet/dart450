22dfa36ac98361a3f2c98eb04c69abdf7af018ab
emilielbrunet <emilie.brunet.97@gmail.com>
2018-01-23 17:30:31 -0500

    added wires to document

---
+++ Exercises/exercise03/js/script.js
@@ -0,0 +1,37 @@
+/**********************************************
+
+DART 450, Winter 2018
+Exercise 3
+Émilie Brunet
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+//setInterval(function (){
+//	var randomNumber = 1 + Math.floor(Math.random() * 16);
+//	$('#element'+ randomNumber).not('visible').show().addClass('visible');
+//	
+//},400);
+//	
+//	});
+//	
+//var hours = 0;
+//	var minutes = 0;
+//	var seconds = 0;
+//
+//setInterval(function () {
+//	seconds = seconds + 1;
+//	if (seconds === 60) {
+//		seconds = 0;
+//		minutes = minutes + 1;
+//	}
+//	if (minutes === 60) {
+//		minutes = 0;
+//		hours = hours + 1;
+//	}
+//	$('#time').text(hours + ":" + ":" + seconds);
+//},100);
+//
+//});
