655fff652e922618a47dbe8af2b8ed23f44ee751
emilielbrunet <emilie.brunet.97@gmail.com>
2018-01-24 17:40:31 -0500

    made a lot of changes

--- Exercises/exercise03/js/script.js
+++ Exercises/exercise03/js/script.js
@@ -9,29 +9,37 @@ Description of what the script does...
 **********************************************/
 
 $(document).ready(function () {
-//setInterval(function (){
-//	var randomNumber = 1 + Math.floor(Math.random() * 16);
-//	$('#element'+ randomNumber).not('visible').show().addClass('visible');
-//	
-//},400);
-//	
-//	});
-//	
-//var hours = 0;
-//	var minutes = 0;
-//	var seconds = 0;
-//
-//setInterval(function () {
-//	seconds = seconds + 1;
-//	if (seconds === 60) {
-//		seconds = 0;
-//		minutes = minutes + 1;
-//	}
-//	if (minutes === 60) {
-//		minutes = 0;
-//		hours = hours + 1;
-//	}
-//	$('#time').text(hours + ":" + ":" + seconds);
-//},100);
-//
-//});
+
+	function close_window() {
+  if (confirm("Close Window?")) {
+    close();
+  }
+}
+
+	});
+
+setInterval(function (){
+	$('#element1, #element2, #element3, #element4, #element5, #element6, #element7').not('visible').show().addClass('visible');
+	
+},400);
+	
+	});
+	
+var hours = 0;
+	var minutes = 0;
+	var seconds = 0;
+
+setInterval(function () {
+	seconds = seconds + 1;
+	if (seconds === 60) {
+		seconds = 0;
+		minutes = minutes + 1;
+	}
+	if (minutes === 60) {
+	minutes = 0;
+		hours = hours + 1;
+	}
+$('#time').text(hours + ":" + ":" + seconds);
+},100);
+
+});
