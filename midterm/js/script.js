79baac750c609a4e0360f69229a910bae3d1bc14
emilielbrunet <emilie.brunet.97@gmail.com>
2018-02-23 10:09:12 -0500

    added boxes to midterm

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -27,93 +27,93 @@ $elements.each(function(i) {
     });
 });
 	
-///////////////	re-sizable element
-	
-	interact('.resize-drag')
-  .draggable({
-    onmove: window.dragMoveListener,
-    restrict: {
-      restriction: 'parent',
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-  })
-  .resizable({
-    edges: { left: true, right: true, bottom: true, top: true },
-
-    restrictEdges: {
-      outer: 'parent',
-      endOnly: true,
-    },
-
-    restrictSize: {
-      max: { width: 800, height: 400 }
-    },
-
-    inertia: true,
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-  });
-	
-///////////////	Rectangle draw tool
-
-initDraw(document.getElementById('canvas'));
-
-
-
-function initDraw(canvas) {
-    function setMousePosition(e) {
-        var ev = e || window.event;
-        if (ev.pageX) {
-            mouse.x = ev.pageX + window.pageXOffset;
-            mouse.y = ev.pageY + window.pageYOffset;
-        } else if (ev.clientX) {
-            mouse.x = ev.clientX + document.body.scrollLeft;
-            mouse.y = ev.clientY + document.body.scrollTop;
-        }
-    }
-
-    var mouse = {
-        x: 0,
-        y: 0,
-        startX: 0,
-        startY: 0
-    };
-    var element = null;
-
-    canvas.onmousemove = function (e) {
-        setMousePosition(e);
-        if (element !== null) {
-            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-        }
-    };
-
-    canvas.onclick = function (e) {
-        if (element !== null) {
-            element = null;
-            canvas.style.cursor = "default";
-            console.log("finsihed.");
-        } else {
-            console.log("begun.");
-            mouse.startX = mouse.x;
-            mouse.startY = mouse.y;
-            element = document.createElement('div');
-            element.className = 'rectangle';
-            element.style.left = mouse.x + 'px';
-            element.style.top = mouse.y + 'px';
-            canvas.appendChild(element);
-            canvas.style.cursor = "crosshair";
-        }
-    };
-}
+/////////////////	re-sizable element
+//	
+//	interact('.resize-drag')
+//  .draggable({
+//    onmove: window.dragMoveListener,
+//    restrict: {
+//      restriction: 'parent',
+//      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+//    },
+//  })
+//  .resizable({
+//    edges: { left: true, right: true, bottom: true, top: true },
+//
+//    restrictEdges: {
+//      outer: 'parent',
+//      endOnly: true,
+//    },
+//
+//    restrictSize: {
+//      max: { width: 800, height: 400 }
+//    },
+//
+//    inertia: true,
+//  })
+//  .on('resizemove', function (event) {
+//    var target = event.target,
+//        x = (parseFloat(target.getAttribute('data-x')) || 0),
+//        y = (parseFloat(target.getAttribute('data-y')) || 0);
+//
+//    target.style.width  = event.rect.width + 'px';
+//    target.style.height = event.rect.height + 'px';
+//  });
+//	
+/////////////////	Rectangle draw tool
+//
+//initDraw(document.getElementById('canvas'));
+//
+//
+//
+//function initDraw(canvas) {
+//    function setMousePosition(e) {
+//        var ev = e || window.event;
+//        if (ev.pageX) {
+//            mouse.x = ev.pageX + window.pageXOffset;
+//            mouse.y = ev.pageY + window.pageYOffset;
+//        } else if (ev.clientX) {
+//            mouse.x = ev.clientX + document.body.scrollLeft;
+//            mouse.y = ev.clientY + document.body.scrollTop;
+//        }
+//    }
+//
+//    var mouse = {
+//        x: 0,
+//        y: 0,
+//        startX: 0,
+//        startY: 0
+//    };
+//    var element = null;
+//
+//    canvas.onmousemove = function (e) {
+//        setMousePosition(e);
+//        if (element !== null) {
+//            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+//            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+//            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+//            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+//        }
+//    };
+//
+//    canvas.onclick = function (e) {
+//        if (element !== null) {
+//            element = null;
+//            canvas.style.cursor = "default";
+//            console.log("finsihed.");
+//        } else {
+//            console.log("begun.");
+//            mouse.startX = mouse.x;
+//            mouse.startY = mouse.y;
+//            element = document.createElement('div');
+//            element.className = 'rectangle';
+//            element.style.left = mouse.x + 'px';
+//            element.style.top = mouse.y + 'px';
+//            canvas.appendChild(element);
+//            canvas.style.cursor = "crosshair";
+//        }
+//    };
+//}
 	
 	});
 	
