6f00eebb85fdec54590fb5b5cf97b65691aa1080
emilielbrunet <emilie.brunet.97@gmail.com>
2018-02-23 23:48:33 -0500

    Created Mad lib and started pixel art

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -10,110 +10,43 @@ Midterm
 
 $(document).ready(function() {
 
-var type = 1,
-    radius = '12em',
-    start = -90,
-    $elements = $('.bug:not(:first-child)'),
-    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1,
-    slice = 360 * type / numberOfElements;
+  function setPixelColour(pixel)
+  {
+    pixel.style.backgroundColor = 'penColour';
+  }
 
-$elements.each(function(i) {
-    var $self = $(this),
-        rotate = slice * i + start,
-        rotateReverse = rotate * -1;
-    
-    $self.css({
-        'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
-    });
+  var penColour = 'black';
+
+  function setPenColour(pen)
+  {
+    penColour = pen;
+  }
+
+window.onload = function () {
+        var name = document.querySelector("#name");
+        var place = document.querySelector("#place");
+        var friend = document.querySelector("#friend");
+        var goal = document.querySelector("#goal");
+        launch.addEventListener("click", writeStory, false);
+
+        function writeStory(){
+            var launchedStory = "";
+            launchedStory += "<p>A day in the life of " + name.value + "!<p>";
+            launchedStory += "<p>Every day " + name.value + " wakes up in the " + place.value + " with only one goal in mind, to " + goal.value +
+            ". However " + name.value + " has never been able to " + goal.value + " ever ever not ever. Leaving the " + place.value +
+            ", " + name.value + " starts out into the world, excited to " + goal.value + " . On the way there, " + name.value +
+            " runs into " + friend.value + ". " + friend.value + " is so funny and distracts " + name.value +
+            " with clever wit for hours. By the time " +  name.value +
+            " stops laughing, the sun is already setting and pretty soon it is too late to " + goal.value + ". " + name.value +
+            " finds that another day has passed without the chance to " + goal.value + " and walks back to the " + place.value +
+            ", looking forward to the next day, hoping to finally get to " + goal.value + ".</p>";
+
+           story.innerHTML = launchedStory;
+        }
+}
+
+$('#launch').click(function(){
+              $('#prompt').fadeOut(500);
 });
-	
-/////////////////	re-sizable element
-//	
-//	interact('.resize-drag')
-//  .draggable({
-//    onmove: window.dragMoveListener,
-//    restrict: {
-//      restriction: 'parent',
-//      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-//    },
-//  })
-//  .resizable({
-//    edges: { left: true, right: true, bottom: true, top: true },
-//
-//    restrictEdges: {
-//      outer: 'parent',
-//      endOnly: true,
-//    },
-//
-//    restrictSize: {
-//      max: { width: 800, height: 400 }
-//    },
-//
-//    inertia: true,
-//  })
-//  .on('resizemove', function (event) {
-//    var target = event.target,
-//        x = (parseFloat(target.getAttribute('data-x')) || 0),
-//        y = (parseFloat(target.getAttribute('data-y')) || 0);
-//
-//    target.style.width  = event.rect.width + 'px';
-//    target.style.height = event.rect.height + 'px';
-//  });
-//	
-/////////////////	Rectangle draw tool
-//
-//initDraw(document.getElementById('canvas'));
-//
-//
-//
-//function initDraw(canvas) {
-//    function setMousePosition(e) {
-//        var ev = e || window.event;
-//        if (ev.pageX) {
-//            mouse.x = ev.pageX + window.pageXOffset;
-//            mouse.y = ev.pageY + window.pageYOffset;
-//        } else if (ev.clientX) {
-//            mouse.x = ev.clientX + document.body.scrollLeft;
-//            mouse.y = ev.clientY + document.body.scrollTop;
-//        }
-//    }
-//
-//    var mouse = {
-//        x: 0,
-//        y: 0,
-//        startX: 0,
-//        startY: 0
-//    };
-//    var element = null;
-//
-//    canvas.onmousemove = function (e) {
-//        setMousePosition(e);
-//        if (element !== null) {
-//            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-//            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-//            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-//            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-//        }
-//    };
-//
-//    canvas.onclick = function (e) {
-//        if (element !== null) {
-//            element = null;
-//            canvas.style.cursor = "default";
-//            console.log("finsihed.");
-//        } else {
-//            console.log("begun.");
-//            mouse.startX = mouse.x;
-//            mouse.startY = mouse.y;
-//            element = document.createElement('div');
-//            element.className = 'rectangle';
-//            element.style.left = mouse.x + 'px';
-//            element.style.top = mouse.y + 'px';
-//            canvas.appendChild(element);
-//            canvas.style.cursor = "crosshair";
-//        }
-//    };
-//}
-	
-	});
-	
+
+});
