9fe6d99e1c18f2ba54c2a07ac89cd7ddae202bc0
emilielbrunet <emilie.brunet.97@gmail.com>
2018-03-27 15:46:41 -0400

    Trying to make Annyang work

--- Exercises/exercise08/js/script.js
+++ Exercises/exercise08/js/script.js
@@ -13,94 +13,33 @@ var popSound;
 
 $(document).ready(function() {
 
-//Yell
+// Yell
 //
-//Set variable
+// Set variable
 balloonPop();
 
   var commands = {
-    // If annyang hears "red" it will call makeDivsRed()
+    // If annyang hears "ahhh!" it will call baloonPop()
     'ahhh!': balloonPop,
   };
 
-  // Add commands to annyang!
+  // Add commands to annyang
   annyang.addCommands(commands);
 
-  // Tell annyang! to listen
+  // Tell annyang to listen
   annyang.start();
 }
-
 });
 
-// addSquare
-//
-// Called on command 'add square', appends a new square to the page
-// in a random location
-function addSquare () {
-// Create the square
-$square = $('<div class="square"></div>');
-
-// Choose a random location
-var x = Math.random() * $(window).width();
-var y = Math.random() * $(window).height();
-
-// Set the square's location
-$square.css({
-  top: y + 'px',
-  left: x + 'px'
-})
-
-// Add it to the page
-$('body').append($square);
-}
-
-
-// makeDivsRed
-//
-// Called on command 'red', just sets the CSS appropriately.
-function makeDivsRed () {
-$('.square').css({
-  'background-color': 'red'
-})
-}
-
-// makeDivsGreen
-//
-// Called on command 'green', just sets the CSS appropriately.
-function makeDivsGreen () {
-$('.square').css({
-  'background-color': 'green'
-})
-}
-
-// moveDivsLeft
-//
-// Called on 'move left', animates all .square elements to the left
-function moveDivsLeft() {
-console.log("Left");
-$('.square').animate({
-  left: '-=100px'
-},500);
-}
-
-// moveDivsLeft
-//
-// Called on 'move right', animates all .square elements to the right
-function moveDivsRight() {
-$('.square').animate({
-  left: '+=100px'
-},500);
-}
-
-//SOUNDS
-//
 //Set Sound effect variables (popping sound)
 popSound = new Audio('sound/pop.flac');
   });
 
-	function balloonPop(event, ui) {
-//Remove the balloon
-			$('#balloon').remove();
-//Start playing the popping sound
-			popSound.play();
+// Call the function to make the balloon pop
+function balloonPop () {
+
+// Add pop class to make div grow and disappear
+        $("#balloon").addClass("pop");
+        //Start playing the popping sound
+        			popSound.play();
 }
