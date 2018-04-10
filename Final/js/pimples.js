9827ce855fb1f172b17bdfa345c2be7e09c5386b
emilielbrunet <emilie.brunet.97@gmail.com>
2018-04-10 15:05:24 -0400

    making a bunch of zits

--- Final/js/pimples.js
+++ Final/js/pimples.js
@@ -12,6 +12,24 @@ var popSound;
 
 $(document).ready(function() {
 
+  $("#zit1").delay(200);
+  $("#zit2").delay(300);
+  $("#zit3").delay(100);
+  $("#zit4").delay(400);
+  $("#zit5").delay(100);
+  $("#zit6").delay(350);
+  $("#zit7").delay(100);
+  $("#zit8").delay(250);
+  $("#zit9").delay(350);
+  
   // SOUND
   //Set Sound effect variables (popping sound)
   popSound = new Audio('sound/pop.wav');
+
+  // Set variable
+pimplePop();
+
+// Call the function to make the pimple pop
+function pimplePop () {
+  // Add burst class to make div grow, shake and pop
+          $("#balloon").addClass("burst");
