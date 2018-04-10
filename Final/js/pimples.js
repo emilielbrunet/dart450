d31e1858ff9b934b79209dab4b21be4168523ad7
emilielbrunet <emilie.brunet.97@gmail.com>
2018-04-10 14:39:33 -0400

    Created a pimple!

---
+++ Final/js/pimples.js
@@ -0,0 +1,17 @@
+/**********************************************
+
+DART 450, Winter 2018
+SMUSH
+Émilie Brunet
+
+Library:
+**********************************************/
+
+//Name for our popping balloon sound file
+var popSound;
+
+$(document).ready(function() {
+
+  // SOUND
+  //Set Sound effect variables (popping sound)
+  popSound = new Audio('sound/pop.wav');
