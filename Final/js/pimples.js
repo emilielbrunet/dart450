f98b12a1286cfe0fc1e9410c85cff61b31692355
emilielbrunet <emilie.brunet.97@gmail.com>
2018-04-10 15:40:11 -0400

    added js to zits
    added mouse down and mouse up functions

--- Final/js/pimples.js
+++ Final/js/pimples.js
@@ -12,24 +12,31 @@ var popSound;
 
 $(document).ready(function() {
 
-  $("#zit1").delay(200);
-  $("#zit2").delay(300);
-  $("#zit3").delay(100);
-  $("#zit4").delay(400);
-  $("#zit5").delay(100);
-  $("#zit6").delay(350);
-  $("#zit7").delay(100);
-  $("#zit8").delay(250);
-  $("#zit9").delay(350);
-  
-  // SOUND
-  //Set Sound effect variables (popping sound)
-  popSound = new Audio('sound/pop.wav');
+$("#zit1").delay(200);
+$("#zit2").delay(300);
+$("#zit3").delay(100);
+$("#zit4").delay(400);
+$("#zit5").delay(100);
+$("#zit6").delay(350);
+$("#zit7").delay(100);
+$("#zit8").delay(250);
+$("#zit9").delay(350);
 
-  // Set variable
-pimplePop();
+// SOUND
+//Set Sound effect variables (popping sound)
+popSound = new Audio('sound/squish-wet.wav');
 
-// Call the function to make the pimple pop
-function pimplePop () {
-  // Add burst class to make div grow, shake and pop
-          $("#balloon").addClass("burst");
+$('.pimple').on('mousedown',function() {
+  $(this).effect('shake', {
+    times: 10,
+    distance: 1.5
+  },750, function() {
+    popSound.play();
+    $(this).hide
+  });
+});
+
+$('.pimple').on('mouseup',function() {
+  $(this).finish();
+});
+});
