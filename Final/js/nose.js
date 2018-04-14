ee617bdf975366bef70cd23f4d112decb56341b1
emilielbrunet <emilie.brunet.97@gmail.com>
2018-04-14 14:44:09 -0400

    fixed nose scroll with help from Pippin

--- Final/js/nose.js
+++ Final/js/nose.js
@@ -8,47 +8,42 @@ Libraries:
 Waypoints
 http://imakewebthings.com/waypoints
 
+
 jQuery Easing Plugin
 http://gsgd.co.uk/sandbox/jquery/easing/
 **********************************************/
 jQuery.easing.def = "string";
 
 $(document).ready(function () {
-//On page load start making snot drip by targeting height, width and opacity
-      $('#trigger').delay(500).animate({
-        minHeight: '350px',
-        minWidth: '35px',
-        opacity: '1'
-      },4000);
+  //On page load start making snot drip by targeting height, width and opacity
+  $('#trigger').delay(500).animate({
+    minHeight: '45vh',
+    minWidth: '35px',
+    opacity: '1'
+  },5000);
 
-      // $(document).animate({
-      //   scrollTop: '15000px'
-      // },1);
+  // Scroll to the bottom of the page on after a quick delay to avoid problems
+  setTimeout(function () {
+    $(document).scrollTop(1500);
+  },1);
 
-// setTimeout(function() {
-//   $(document).scrollTop(11000);
-// },1);
-
-  // Create a waypoint associated with #trigger
-  // Set function name to 'handler' (called when we scroll to the element)
-  // Set the 'offset' to 50% between the trigger and the top of the page
-  $('#trigger').waypoint({
-    handler: handleTrigger,
-    offset: '50%'
-  });
+  // Listen for scrolling events so we can react
+  $(document).on('scroll',handleScroll);
 });
 
-// handleTrigger
-//
-// If we're scrolling down to the element it gets taller,
-// if we're scrolling up to the element it gets smaller and disapears
-function handleTrigger(direction) {
 
-  if (direction == 'down') {
+function handleScroll() {
+  // If they scrolled back to the top of the page
+  if ($(document).scrollTop() ===  0) {
+    // Stop any current animation (like the snot going down)
+    $('#trigger').stop();
+    // Animate the snot back in
     $('#trigger').animate({
       minHeight: '0px',
-      minWidth: '30px',
+      minWidth: '0px',
       opacity: '0'
-    });
+    },500);
+    // Play a sound?
+    // Go to the next thing?
   }
 }
