9a975fdaa1efae1eda35588154f41283516dc3aa
emilielbrunet <emilie.brunet.97@gmail.com>
2018-04-17 21:22:04 -0400

    working on plate drop js

--- Final/js/blue.js
+++ Final/js/blue.js
@@ -10,11 +10,11 @@ Plugin for mobile touch drag and drop http://touchpunch.furf.com/)
 
 
 var scrapeSound;
-var eatSound;
+var smashSound;
 
 //function to open next page with delay
 				function pageLoad() {
-					window.location.assign("balloon.html");
+					window.location.assign("end.html");
 				}
 
 $(document).ready(function() {
@@ -23,33 +23,28 @@ $(document).ready(function() {
 //
 //Set Sound effect variables (fly buzzing and eating sound)
 flySound = new Audio('sound/fork.wav');
-eatSound = new Audio('sound/eating_1.mp3');
-
-//Autoplay fly sound on page load
-flySound.play();
+eatSound = new Audio('sound/smash.wav');
 
 //Make the fly draggable
-    $( "#fly" ).draggable();
+    $( "#plate1" ).draggable();
 
 //Make the mouth a droppable target
 //Add an event handler function for 'drop' called flyDrop
-    $( "#teeth" ).droppable({
-      drop: flyDrop
+    $( "#floor" ).droppable({
+		drop: plateDrop
 		});
 // flyDrop
 //
 
   });
 
-	function flyDrop(event, ui) {
+	function plateDrop(event, ui) {
 // Make the mouth close
-			$('#mouth').css('background-image','url(images/mouth_closed.png)');
-//Remove the fly
-			$('#fly').remove();
-//Start playing the new audio
-			eatSound.play();
-//Stop playing the autoplay audio
-			flySound.pause();
+			$(this).css('background-image','url(images/smashed1.png)');
+//Start playing the smash audio
+			smashSound.play();
+//Start playing the fork audio
+			forkSound.play();
 //delay the function called pageLoad by 1.5 seconds and then go to new page
       setTimeout(pageLoad,1000)
 }
